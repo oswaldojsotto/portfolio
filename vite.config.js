2993db0206a922429c41038e03f50674b4f22967
Oswaldo <oswaldojsotto@gmail.com>
2024-02-28 04:07:32 -0400

    vercelfixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "@vuetify/vite-plugin";
+
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/",
+  plugins: [react(), vue(), vuetify({ autoImport: true })],
+  define: { "process.env": {} },
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+  base: "./",
 });
